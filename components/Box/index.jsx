e6998dd721c08761e63cee4359df82558d1e7185
VinhVu <vinh.vu@eastagile.com>
2021-12-19 08:50:19 +0700

    [c] Remove unnecessary box wrapper
    use css gap for spacing between boxes

--- components/Box/index.jsx
+++ components/Box/index.jsx
@@ -4,14 +4,12 @@ import styles from './index.module.css'
 
 export default function Box ({ children, onClick, color }) {
   return (
-    <div className={styles.wrapper}> 
-      <button
-        className={styles.box}
-        style={{ backgroundColor: color }}
-        onClick={onClick}
-      > 
-        { children }
-      </button>
-    </div>
+    <button
+      className={styles.box}
+      style={{ backgroundColor: color }}
+      onClick={onClick}
+    > 
+      { children }
+    </button>
   )
 }
